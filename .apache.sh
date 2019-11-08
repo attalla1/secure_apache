847e787175ac6ca1956bbbf6f845754756edbe0c
midodany11 <37474787+midodany11@users.noreply.github.com>
2019-11-09 02:25:55 +0400

    Update .apache.sh

--- .apache.sh
+++ .apache.sh
@@ -6,7 +6,7 @@
 echo >> /etc/apache2/apache2.conf;
 echo "# Prevent Slow HTTP DOS" >> /etc/apache2/apache2.conf;
 echo "<IfModule mod_reqtimeout.c>" >> /etc/apache2/apache2.conf;
-echo " <RequestReadTimeout header=1,MinRate=500 body=1,MinRate=500>" >> /etc/apache2/apache2.conf;
+echo " RequestReadTimeout header=1,MinRate=500 body=1,MinRate=500" >> /etc/apache2/apache2.conf;
 echo "</IfModule>" >> /etc/apache2/apache2.conf;
 sudo systemctl restart apache2.service;
 echo;
