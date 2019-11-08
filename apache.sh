aa7de606a03ab8c16ba497c689c620902f90d77a
midodany11 <37474787+midodany11@users.noreply.github.com>
2019-11-08 23:27:53 +0400

    Create apache.sh

---
+++ apache.sh
@@ -0,0 +1,12 @@
+# Ahmed Attalla
+# Secure_Apache 1.0
+# Add lines to apache2 config file to limit the readtimout 
+
+#!/bin/bash
+
+echo "<IfModule mod_reqtimeout.c>" >> /etc/apache2/apache.conf;
+echo "< RequestReadTimeout header=1,MinRate=500 body=1,MinRate=500>" >> /etc/apache2/apache.conf;
+echo "</IfModule>" >> /etc/apache2/apache.conf;
+echo
+echo "Success!"
+echo
