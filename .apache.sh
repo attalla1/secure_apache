3eabde23beff243f8ff11e081b6f92ab749ee408
midodany11 <37474787+midodany11@users.noreply.github.com>
2019-11-09 01:53:26 +0400

    Update .apache.sh

--- .apache.sh
+++ .apache.sh
@@ -4,9 +4,9 @@
 
 #!/bin/bash
 
-echo "<IfModule mod_reqtimeout.c>" >> /etc/apache2/apache.conf;
-echo "< RequestReadTimeout header=1,MinRate=500 body=1,MinRate=500>" >> /etc/apache2/apache.conf;
-echo "</IfModule>" >> /etc/apache2/apache.conf;
+echo "<IfModule mod_reqtimeout.c>" >> /etc/apache2/apache2.conf;
+echo "< RequestReadTimeout header=1,MinRate=500 body=1,MinRate=500>" >> /etc/apache2/apache2.conf;
+echo "</IfModule>" >> /etc/apache2/apache2.conf;
 sudo systemctl restart apache2.service;
 echo;
 echo "Success!";
