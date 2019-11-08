a1f763d00b6d5a43caaf6d034687674ac0e21ca4
midodany11 <37474787+midodany11@users.noreply.github.com>
2019-11-09 01:38:09 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Ahmed Attalla
-# Secure_Apache 1.0
-cat << "EOF"
+echo;
+echo;
+cat << "EOF";
+
                      .- . _
             .:       `      " - . _
      \     ::          `            ""--..__                                                . . . :  :  :
@@ -28,22 +29,29 @@ cat << "EOF"
                                    :          :
                                  ""--..__     :
                                          ""--.:_
-                                                ""--..__.
-                                                
-                                Secure_Apatche 1.0 | © Ahmed Attalla 2019 ©
+                                                ""--..___  
+                                              
+                                Secure_Apatche 1.0 | Â© Ahmed Attalla 2019 Â©
 
-EOF;
+EOF
 sleep 6;
+clear;
 echo;
-echo "Note: Hopefully you are a super-user otherwise this script will not work";
+echo "WARNING: This tool assumes that you have already apache2 installed and located at '/etc/apache2'";
 echo;
+echo "* If you don't have apache2 installed you can install it using 'apt install apache2'";
 echo;
-echo "This tool will:" Add reasonable request_timeout value to apache2 configuration file. install fail2ban and configure it";
+sleep 15;
+clear;
+echo;
+echo "This tool will:";
 echo;
 echo "* Add reasonable request_timeout value to apache2 configuration file.";
-echo "* Install fail2ban tool and configure it to block any http_dos attack";
+echo "* Install fail2ban tool and configure it to Block any http_dos attack.";
 echo;
-sleep 7;
+echo;
+sleep 10;
+clear;
 # Update & Upgrade
 apt-get update -y;
 apt-get upgrade -y;
@@ -54,19 +62,49 @@ chmod +x *;
 echo;
 echo "Apache is now Safe!";
 echo;
+sleep 3;
+clear;
+echo;
 echo "Note: Fail2Ban will be installed now, if you DO NOT want to install it, press ctr+c to exit"
 echo;
-sleep 7;
-
+sleep 10;
+clear;
 # Install Fail2Ban
-sudo apt-get install fail2ban;
+sudo apt-get install fail2ban -y;
 sudo systemctl start fail2ban;
 sudo systemctl enable fail2ban;
 cp f2b_local_jail /etc/fail2ban/jail.local;
 cp http-get-dos /etc/fail2ban/filter.d/http-get-dos.conf;
 sudo systemctl restart fail2ban;
 echo;
+clear;
+echo;
+echo;
+cat << "EOF"
+
+                     `. ___
+                    __,' __`.                _..----....____
+        __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'
+  _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'
+,'________________                          \`-._`-','
+ `._              ```````````------...___   '-.._'-:
+    ```--.._      ,.                     ````--...__\-.
+            `.--. `-`                       ____    |  |`
+              `. `.                       ,'`````.  ;  ;`
+                `._`.        __________   `.      \'__/`
+                   `-:._____/______/___/____`.     \  `
+                               |       `._    `.    \
+                               `._________`-.   `.   `.___
+                                             SSt  `------'`
+
+
+            Secure_Apatche 1.0 | Â© Ahmed Attalla 2019 Â©
+
+EOF
+echo;
+echo;
 echo "Success!";
+echo;
 echo;
 echo "Fail2Ban Installed!, Now you can configure rules at /etc/fail2ban/jail.local";
 echo;
@@ -76,5 +114,7 @@ echo "use 'sudo fail2ban-client status <jail_name>' to view details for a specif
 echo;
 echo "use 'sudo fail2ban-client set <jail_name> unbanip <ip_address>' to unblock an ip";
 echo;
-echo "You are good to go!"
+echo;
+echo "You are good to go!";
+echo;
 echo;
