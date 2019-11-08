b0efd8fddd21d7de370a8c79f49d1e99e73dd18c
midodany11 <37474787+midodany11@users.noreply.github.com>
2019-11-09 01:55:59 +0400

    Update .apache.sh

--- .apache.sh
+++ .apache.sh
@@ -3,7 +3,8 @@
 # Add lines to apache2 config file to limit the readtimout 
 
 #!/bin/bash
-
+echo >> /etc/apache2/apache2.conf;
+echo "# Prevent Slow HTTP DOS" >> /etc/apache2/apache2.conf;
 echo "<IfModule mod_reqtimeout.c>" >> /etc/apache2/apache2.conf;
 echo "< RequestReadTimeout header=1,MinRate=500 body=1,MinRate=500>" >> /etc/apache2/apache2.conf;
 echo "</IfModule>" >> /etc/apache2/apache2.conf;
